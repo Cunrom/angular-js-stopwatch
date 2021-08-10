cd12e4860dd9c2546d7e009fe0620376ea9fd456
Cunrom <johntruong06@gmail.com>
2021-08-10 18:29:03 -0400

    Added sound effects

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -11,29 +11,39 @@ export class StopwatchComponent implements OnInit {
   canReset = false;
   seconds = 0;
   minutes = 0;
+  clockTick = new Audio('../../assets/audio/clock-tick.mp3');
+  mouseClick = new Audio('../../assets/audio/mouse-click.mp3');
   constructor() { }
 
   ngOnInit(): void {
   }
 
   startTimer() {
+    this.mouseClick.play();
     this.started = true;
     setTimeout(() => {
       this.canReset = true;
     }, 1000);
     this.myInterval = setInterval(() => {
       this.seconds += 1;
+      this.clockTick.play();
       if (this.seconds === 60) {
         this.seconds = 0;
         this.minutes += 1;
       }
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.seconds = 0;
+      }
     }, 1000);
   }
   stopTimer() {
+    this.mouseClick.play();
     this.started = false;
     clearInterval(this.myInterval);
   }
   resetTimer() {
+    this.mouseClick.play();
     clearInterval(this.myInterval);
     this.canReset = false;
     this.started = false;
